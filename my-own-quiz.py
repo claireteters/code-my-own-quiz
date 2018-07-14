5b6f3323732318eea614a2a508ea238f8cca78dd
Claire Teters <claire@Claires-MacBook-Pro.local>
2018-07-14 14:30:20 -0700

    fixed how i was replacing the answer number with the user imput. i also got rid of leaving the underscores around the answer after it was replaced in the quiz question. it all seems to work until i get passed question number 2 for any level quiz choosen. after that  it stops printing the quiz and replacing the quiz questions with the quiz answer.

--- my-own-quiz.py
+++ my-own-quiz.py
@@ -1,10 +1,10 @@
 # Opening prompts and selecting game level
 def set_game_difficulty():
-    print "Let's take a quiz!"
+    print "Let's take a quiz about the hit TV series, 'The Office'!\n"
     choosen_level = raw_input("Please type a game difficulty of easy, medium, or hard: ")
     if choosen_level == "easy" or choosen_level == "medium" or choosen_level == "hard":
-        print "Let's do a " + choosen_level + " level!"
-        print "Your quiz begins below!"
+        print "Let's do a " + choosen_level + " level!\n"
+        print "Your quiz begins below!\n"
     else:
         print "Please imput a valid answer of easy, medium, or hard."
     return choosen_level
@@ -12,21 +12,21 @@ def set_game_difficulty():
 #After selecting which game they woud like to play
 def game_questions(choosen_level):
     if choosen_level == "easy":
-        quiz = '''A __1__ is created with the def keyword. You specify the inputs a __1__ takes by adding __2__ separated by commas between the parentheses. __1__s by default return __3__ if you don't specify the value to return. __2__ can be standard data types such as string, number, dictionary, tuple, and __4__ or can be more complicated such as objects and lambda functions.'''
+        quiz = '''In The Office, there are numerous characters to keep track of. __1__ is the boss of the Scranton branch of Dunder Mifflin. __1__'s number one assistant to the regional manager is __2__, who is also a beet farmer on the side. __3__ is the receptionist who is regularly visited by __4__. __4__ is completely in love with __3__, which makes for an excelent story line throughout the show.'''
     elif choosen_level == "medium":
-        quiz = '''We used to dream about this stuff. Now, we get to build it. It's pretty neat. ___1___ was the Chairman and CEO of Apple Inc., a company he founded with ___2___ in ___3___. He was also the CEO of Pixar Animation Studios until it was acquired by the Walt Disney Company in 2007. ___1___ was the Walt Disney Company's largest individual shareholder and a former member of its Board of Directors. He is considered to have been a leading figure in both the computer and entertainment industries. Apple was released ___4___ in 2007. This Product caused a great change in the lives of people. '''
+        quiz = '''In The Office, Michael makes a call to the office screaming about how he __1__ on his George Foreman Grill because he likes to wake up to the smell of bacon in the morning. Dwight decides to save the day by going to his house to get him, and while doing so, __2__ into a pole giving himself a __3__. We know this because later in the day, he starts showing symptoms like typing the same word multiple times on his computer screen, and not knowing hes raising his hand durring a __4__.'''
     elif choosen_level == "hard":
-        quiz = '''__1___ is a sovereign island nation in East Asia.The 2020 Summer Olympics will be held in a capital of ___1___ : ___2___.This country has very traditional city ___3___ is most popular city among foreign tourists. Food in this country is so good especially ___4___ : raw sliced fish, shellfish or crustaceans on vinger rice. This country is birthplace of ___5___. ___5___ is pretty popular among teenager around the world. This country is facing a declining birth rate and an aging population. People of 65 and above are over ___6___ mollion people in ___1___.'''
+        quiz = '''On the episode "Goodbye Michael" of The Office, the group __1__ to him. Michael is caught off gaurd by this and whispers to himself, "__2__". They tell him that he has worked there for so long, that it's like watching the movie 'Die Hard' __3__ times. Meredith gets a solo saying you hit me with your __4__, and Creed says I __5__ you when you __6__. They all join together near the end and tell Michael, remember to __7__!'''
     return quiz
         
 #Depending on which level they select, the answers of that game are here.
 def game_answers(choosen_level):
     if choosen_level == "easy":
-        answer = ["function", "parameters", "errors", "numbers"]
+        answer = ["Michael", "Dwight", "Pam", "Jim"]
     elif choosen_level == "medium":
-        answer = ["Steven Paul Jobs", "Steve Wozniak", "1976", "iPhone"]
+        answer = ["stepped", "crashes", "concussion", "meeting"]
     elif choosen_level == "hard":
-        answer = ["Japan", "Tokyo", "Kyoto", "sushi", "anime", "30"]
+        answer = ["sings", "something is happening", "eighty thousand", "car", "watch", "sleep", "call"]
     return answer
 
 #Goes through the quiz being taken and makes each word an individual string,
@@ -37,19 +37,17 @@ def word_in_pos(answer_number, split_question):
     number = answer_number
     for number in split_question:
         if number == "__" + str(answer_number) + "__":
-            print number
             return number
-        else:
-            return None
+    return None 
 
 #Fills in the place of the quiz question number that the user is answering
 #with the correct answer of the question being asked. The underscores remain there.
 def answering_questions(question, user_answer, answer_number):
+    question_with_answer = ""
     split_question = question.split()
     number_in_question = word_in_pos(answer_number, split_question)
-    if number_in_question == None:
-        number = str(answer_number)
-        question_with_answer = question.replace(number, user_answer)
+    if number_in_question != None:
+        question_with_answer = question.replace(number_in_question, user_answer)
     return question_with_answer
 
 #Quiz operations
@@ -57,20 +55,18 @@ def play_game():
     choosen_level = set_game_difficulty()
     question = game_questions(choosen_level)
     solutions = game_answers(choosen_level)
-    answers_left = 4
-    print "You will get " + str(len(solutions)) + " guesses per problem."
     print question
     answer_number = 1
     while answer_number <= len(solutions):
         user_answer = raw_input("Type your best guess for __"+ str(answer_number) + "__. \n>>")
         index = answer_number - 1
         if user_answer == solutions[index]:
-            print "Correct!"
+            print "Correct!\n"
             question = answering_questions(question, user_answer, answer_number)
             print question
             answer_number += 1
         else:
-            print "Incorrect. Try again!"
+            print "Incorrect. Try again!\n"
     print "Nice! You Passed the " + choosen_level + " level. You rock!"
 
 play_game()
